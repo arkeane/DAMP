b31ea347375f00bbd64abd0acb983a625115b08f
Ludovico Pestarino <ludovicopestarino00@gmail.com>
2022-10-18 18:45:31 +0200

    homepage

---
+++ www/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>DAMP</title>
+    <link rel='stylesheet' href='style.css'>
+</head>
+
+<body>
+
+    <h1 class=hometitle>DAMP</h1>
+    <h2 class=subtitle>Docker Apache MariaDB PHP Stack</h2>
+
+    <div class=testlinks>
+    <h2>Test Links</h2>
+    <li><a href='phpinfo.php'>PHP info</a>
+    <li><a href='testdb.php'>Test MariaDB connection</a>
+    <li><a href='http://localhost:81/'>phpMyAdmin</a>
+    </div>
+
+</body>
+
+</html>
